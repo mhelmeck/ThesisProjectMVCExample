7d640ead37b92f7438b12d589c3491f32d03e217
Maciej Hełmecki <mhelmecki@gmail.com>
2018-12-23 01:12:59 +0100

    Refactor DetailViewController

--- ThesisProjectMVCExample/Project/Controllers/DetailViewController.swift
+++ ThesisProjectMVCExample/Project/Controllers/DetailViewController.swift
@@ -9,130 +9,66 @@
 import UIKit
 
 public class DetailViewController: UIViewController {
-    // Properties
+    // MARK: - Public properties
     public var forecastCollection = [Forecast]()
     public var cityName: String = ""
     
+    // MARK: - Private properties
+    @IBOutlet private weak var forecastView: ForecastUIView!
     @IBOutlet private weak var dateLabel: UILabel!
-    
-    @IBOutlet private weak var mainViewContainer: UIView!
-    @IBOutlet private weak var imageView: UIImageView!
-    
-    @IBOutlet private weak var typeTitle: UILabel!
-    @IBOutlet private weak var typeValue: UILabel!
-    
-    @IBOutlet private weak var minTempTitle: UILabel!
-    @IBOutlet private weak var minTempValue: UILabel!
-    
-    @IBOutlet private weak var maxTempValue: UILabel!
-    @IBOutlet private weak var maxTempTitle: UILabel!
-    
-    @IBOutlet private weak var windSpeedTitle: UILabel!
-    @IBOutlet private weak var windSpeedValue: UILabel!
-    
-    @IBOutlet private weak var windDirectionTitle: UILabel!
-    @IBOutlet private weak var windDirectionValue: UILabel!
-    
-    @IBOutlet private weak var rainfallTitle: UILabel!
-    @IBOutlet private weak var rainfallValue: UILabel!
-    
-    @IBOutlet private weak var pressureTitle: UILabel!
-    @IBOutlet private weak var pressureValue: UILabel!
-    
     @IBOutlet private weak var previewButton: UIButton!
     @IBOutlet private weak var nextButton: UIButton!
     
-    private var shouldEnableButton: ((ButtonType, Bool) -> Void)!
-    
-    private var iterator: Int = 0 {
+    private var forecastIndex: Int = 0 {
         didSet {
-            handleButtons(withIterator: iterator)
+            handleButtons(at: forecastIndex)
+            updateView(withForecast: forecastCollection[forecastIndex])
+            dateLabel.text = forecastCollection[forecastIndex].date
         }
     }
     
-    // Life cycle
+    // MARK: - Init
     override public func viewDidLoad() {
         super.viewDidLoad()
         super.title = cityName
         
         setupView()
-        
-        shouldEnableButton = { [weak self] buttonType, isEnabled in
-            self?.handleButton(withType: buttonType, isEnabled: isEnabled)
-        }
-        
-        iterator = 0
-        
         updateView(withForecast: forecastCollection.first!)
+        
+        forecastIndex = 0
     }
     
-    // Actions
+    // MARK: - Private methods
     @IBAction private func previewButtonTapped(_ sender: Any) {
         perform(animation: {
-            self.mainViewContainer.alpha = 0
+            self.forecastView.alpha = 0
         }, withCompletion: {
-            self.showWeatherForPreviewDay { [weak self] weather in
-                self?.updateView(withForecast: weather)
-            }
+            self.forecastIndex -= 1
         })
         
         perform(animation: {
-            self.mainViewContainer.alpha = 1
+            self.forecastView.alpha = 1
         }, withCompletion: nil)
     }
     
     @IBAction private func nextButtonTapped(_ sender: Any) {
         perform(animation: {
-            self.mainViewContainer.alpha = 0
+            self.forecastView.alpha = 0
         }, withCompletion: {
-            self.showWeatherForNextDay { [weak self] weather in
-                self?.updateView(withForecast: weather)
-            }
+            self.forecastIndex += 1
         })
         
         perform(animation: {
-            self.mainViewContainer.alpha = 1
+            self.forecastView.alpha = 1
         }, withCompletion: nil)
     }
     
-    // Private methods
     private func setupView() {
-        view.backgroundColor = .white
-        
-        [typeTitle,
-         minTempTitle,
-         maxTempTitle,
-         windSpeedTitle,
-         windDirectionTitle,
-         rainfallTitle,
-         pressureTitle].forEach {
-            $0?.textColor = .gray
-        }
-        
-        [typeValue,
-         minTempValue,
-         maxTempValue,
-         windSpeedValue,
-         windDirectionValue,
-         rainfallValue,
-         pressureValue].forEach {
-            $0?.text = ""
-            $0?.textColor = .customPurple
-        }
-        
         dateLabel.text = "".uppercased()
-        typeTitle.text = "Type: ".uppercased()
-        minTempTitle.text = "Min temperature: ".uppercased()
-        maxTempTitle.text = "Max temperature: ".uppercased()
-        windSpeedTitle.text = "Wind speed: ".uppercased()
-        windDirectionTitle.text = "Wind direction: ".uppercased()
-        rainfallTitle.text = "Rainfall: ".uppercased()
-        pressureTitle.text = "Pressure: ".uppercased()
-        
-        imageView.contentMode = .scaleAspectFit
-        
+
         previewButton.setTitle("Preview", for: .normal)
         nextButton.setTitle("Next", for: .normal)
+        
         [previewButton, nextButton].forEach {
             $0?.setTitleColor(.white, for: .normal)
             
@@ -146,64 +82,43 @@ public class DetailViewController: UIViewController {
     
     private func updateView(withForecast forecast: Forecast) {
         if let image = UIImage(named: AssetCodeMapper.map(forecast.assetCode)) {
-            imageView.image = image
+            forecastView.imageView.image = image
         }
         
         dateLabel.text = forecast.date
-        typeValue.text = forecast.type
-        maxTempValue.text = [String(Int(forecast.maxTemperature)), "°C"].joined(separator: " ")
-        minTempValue.text = [String(Int(forecast.minTemperature)), "°C"].joined(separator: " ")
-        windSpeedValue.text = [String(Int(forecast.windSpeed)), "m/s"].joined(separator: " ")
-        windDirectionValue.text = forecast.windDirection
-        pressureValue.text = [String(Int(forecast.airPressure)), "hPa"].joined(separator: " ")
+        forecastView.typeValue.text = forecast.type
+        forecastView.maxTempValue.text = [String(Int(forecast.maxTemperature)), "°C"].joined(separator: " ")
+        forecastView.minTempValue.text = [String(Int(forecast.minTemperature)), "°C"].joined(separator: " ")
+        forecastView.windSpeedValue.text = [String(Int(forecast.windSpeed)), "m/s"].joined(separator: " ")
+        forecastView.windDirectionValue.text = forecast.windDirection
+        forecastView.pressureValue.text = [String(Int(forecast.airPressure)), "hPa"].joined(separator: " ")
         
         switch forecast.assetCode {
         case "h", "hr", "lr", "s", "t":
-            rainfallValue.text = "It's raining"
+            forecastView.rainfallValue.text = "It's raining"
         default:
-            rainfallValue.text = "It's not raining"
+            forecastView.rainfallValue.text = "It's not raining"
         }
     }
     
-    private func handleButton(withType type: ButtonType, isEnabled: Bool) {
-        switch type {
-        case .next:
-            nextButton.isEnabled = isEnabled
-        case .preview:
-            previewButton.isEnabled = isEnabled
-        }
-    }
-    
-    private func showWeatherForNextDay(completion: @escaping (Forecast) -> Void) {
-        iterator += 1
-        
-        completion(forecastCollection[iterator])
-    }
-    
-    private func showWeatherForPreviewDay(completion: @escaping (Forecast) -> Void) {
-        iterator -= 1
-        
-        completion(forecastCollection[iterator])
-    }
-    
-    private func handleButtons(withIterator iterator: Int) {
-        if iterator == 0 {
-            shouldEnableButton(.preview, false)
-            if iterator != forecastCollection.count - 1 {
-                shouldEnableButton(.next, true)
+    private func handleButtons(at forecastIndex: Int) {
+        if forecastIndex == 0 {
+            previewButton.isEnabled = false
+            if forecastIndex != forecastCollection.count - 1 {
+                nextButton.isEnabled = true
             }
         }
         
-        if iterator == forecastCollection.count - 1 {
-            shouldEnableButton(.next, false)
-            if iterator != 0 {
-                shouldEnableButton(.preview, true)
+        if forecastIndex == forecastCollection.count - 1 {
+            nextButton.isEnabled = false
+            if forecastIndex != 0 {
+                previewButton.isEnabled = true
             }
         }
         
-        if iterator > 0 && iterator < forecastCollection.count - 1 {
-            shouldEnableButton(.preview, true)
-            shouldEnableButton(.next, true)
+        if forecastIndex > 0 && forecastIndex < forecastCollection.count - 1 {
+            previewButton.isEnabled = true
+            nextButton.isEnabled = true
         }
     }
     
